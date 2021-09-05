08a3ba526acd39cf8539e5841b73a81d34804518
villagemc <46008022+villagemc@users.noreply.github.com>
2021-09-05 15:48:55 +0300

    Add files via upload

---
+++ GroupVMC/language.js
@@ -0,0 +1,216 @@
+let descProject1 = document.querySelector('.descProject1');
+let descProject2 = document.querySelector('.descProject2');
+let contact = document.querySelector('.contact')
+
+let Razban = document.querySelector('.Razban');
+let Razban1 = document.querySelector('.Razban1');
+let Razban2 = document.querySelector('.Razban2');
+let Razban3 = document.querySelector('.Razban3');
+let Razban4 = document.querySelector('.Razban4');
+
+let Razmut = document.querySelector('.Razmut');
+let Razmut1 = document.querySelector('.Razmut1');
+let Razmut2 = document.querySelector('.Razmut2');
+let Razmut3 = document.querySelector('.Razmut3');
+let Razmut4 = document.querySelector('.Razmut4');
+
+let Perenos = document.querySelector('.Perenos');
+let Perenos1 = document.querySelector('.Perenos1');
+let Perenos2 = document.querySelector('.Perenos2');
+let Perenos3 = document.querySelector('.Perenos3');
+let Perenos4 = document.querySelector('.Perenos4');
+
+let RazHS = document.querySelector('.RazHS');
+let RazHS1 = document.querySelector('.RazHS1');
+let RazHS2 = document.querySelector('.RazHS2');
+let RazHS3 = document.querySelector('.RazHS3');
+let RazHS4 = document.querySelector('.RazHS4');
+
+let Donat = document.querySelector('.Donat');
+let Donat1 = document.querySelector('.Donat1');
+let Donat2 = document.querySelector('.Donat2');
+let Donat3 = document.querySelector('.Donat3');
+let Donat4 = document.querySelector('.Donat4');
+
+let VK = document.querySelector('.VK')
+let Сhoice = document.querySelector('.Сhoice')
+
+function Russia() {
+	descProject1.textContent =  'VillageMC - самый добрый сервер,';
+	descProject2.textContent =  'именно поэтому мы даём шанс игрокам на исправление!';
+	contact.textContent = 'Наш магазин!'
+	Razban.textContent =  'Разбан';
+	Razban1.textContent =  'Осознал вину?';
+	Razban2.textContent =  'Хочешь веселья? ';
+	Razban3.textContent =  'Разблокируем тебя';
+	Razban4.textContent =  'за небольшую плату';
+	Razmut.textContent =  'Размут';
+	Razmut1.textContent =  'Снимем мут с';
+	Razmut2.textContent =  'твоего аккаунта!';
+	Razmut3.textContent =  'Общайся в чате';
+	Razmut4.textContent =  'и спокойно играй ';
+	RazHS.textContent =  'РазЧС';
+	RazHS1.textContent =  'Попал в ЧС VK? ';
+	RazHS2.textContent =  'Ничего страшного!';
+	RazHS3.textContent =  'Купи разЧС ';
+	RazHS4.textContent =  'И будь на связи!';
+	Perenos.textContent =  'Перенос';
+	Perenos1.textContent =  'Перенесем данные';
+	Perenos2.textContent =  'и привилегию ';
+	Perenos3.textContent =  'с твоего аккаунта';
+	Perenos4.textContent =  'на другой аккаунт';
+	Donat.textContent =  'Донат';
+	Donat1.textContent =  'Приобрети донат,';
+	Donat2.textContent =  'И среди всех ';
+	Donat3.textContent =  'будь самым';
+	Donat4.textContent =  'крутым на сервере!';
+	VK.textContent = 'Техническая поддержка | VillageMC';
+	Сhoice.textContent = 'Выбор языка';
+}
+
+function Ukraine() {
+	descProject1.textContent = 'VillageMC - найдобріший сервер,';
+	descProject2.textContent = ' саме тому ми даємо шанс гравцям на виправлення!';
+	contact.textContent = ' Наш магазин!'
+	Razban.textContent = 'Розбан';
+	Razban1.textContent = ' усвідомив провину?';
+	Razban2.textContent = ' хочеш веселощів? ';
+	Razban3.textContent = 'розблокуємо тебе';
+	Razban4.textContent = 'за невелику плату';
+	Razmut.textContent = 'Размут';
+	Razmut1.textContent = 'знімемо мут з';
+	Razmut2.textContent = ' твого аккаунта!';
+	Razmut3.textContent = 'спілкуйся в чаті';
+	Razmut4.textContent = 'і спокійно Грай ';
+	RazHS.textContent = 'РазЧС';
+	RazHS1.textContent = ' потрапив в НС VK? ';
+	RazHS2.textContent = ' нічого страшного!';
+	RazHS3.textContent = ' Купи раз ';
+	RazHS4.textContent = "і будь на зв'язку!";
+	Perenos.textContent = 'перенесення';
+	Perenos1.textContent = 'перенесемо дані';
+	Perenos2.textContent = ' і привілей ';
+	Perenos3.textContent = 'з твого аккаунта';
+	Perenos4.textContent = 'на інший аккаунт';
+	Donat.textContent = 'Донат';
+	Donat1.textContent = ' придбай донат,';
+	Donat2.textContent = 'і серед усіх ';
+	Donat3.textContent = 'будь самим';
+	Donat4.textContent = ' крутим на сервері!';
+	VK.textContent = 'Технічна підтримка | VillageMC';
+	Сhoice.textContent = 'вибір мови';
+}
+
+function Kazahstan() {
+	descProject1.textContent = ' VillageMC - ең мейірімді сервер,';
+	descProject2.textContent = ' сондықтан біз ойыншыларға түзетуге мүмкіндік береміз!';
+	contact.textContent = ' біздің дүкен!'
+	Razban.textContent = 'Шотты бұғаттау';
+	Razban1.textContent = ' кінәні түсінді ме?';
+	Razban2.textContent = ' көңілді болғыңыз келе ме? ';
+	Razban3.textContent = 'сізді құлыптан босату';
+	Razban4.textContent = 'аз ақыға';
+	Razmut.textContent = 'Чатты бұғаттау';
+	Razmut1.textContent = 'Mut-тен алыңыз';
+	Razmut2.textContent = ' сіздің есептік жазбаңыз!';
+	Razmut3.textContent = 'чатта сөйлесу';
+	Razmut4.textContent = 'және тыныш ойнаңыз';
+	RazHS.textContent = 'Vk құлпы';
+	RazHS1.textContent = ' VK төтенше жағдайына тап болдыңыз ба? ';
+	RazHS2.textContent = ' жақсы!';
+	RazHS3.textContent = ' textcontent сатып ал ';
+	RazHS4.textContent = ' және байланыста болыңыз!';
+	Perenos.textContent = 'Ауыстыру';
+	Perenos1.textContent = 'деректерді беру';
+	Perenos2.textContent = ' және артықшылық ';
+	Perenos3.textContent = 'сіздің есептік жазбаңыздан';
+	Perenos4.textContent = 'басқа есептік жазбада';
+	Donat.textContent = 'Донат';
+	Donat1.textContent = 'донат сатып ал,';
+	Donat2.textContent = 'және барлық арасында';
+	Donat3.textContent = 'ең көп бол';
+	Donat4.textContent = ' серверде керемет!';
+	VK.textContent = 'техникалық қолдау | VillageMC';
+	Сһоісе.textContent = 'тіл таңдау';
+}
+
+function Uzbekistan() {
+	descProject1.textContent = ' VillageMC-eng yaxshi server,';
+	descProject2.textContent = " biz tuzatish uchun futbolchilar uchun imkoniyat berish nima uchun o'sha!";
+	contact.textContent = " bizning do'kon!";
+	Razban.textContent = "Hisobni ochish";
+	Razban1.textContent = ' aybni amalga oshirganmi?';
+	Razban2.textContent = 'qiziqarli istaysizmi? ';
+	Razban3.textContent = "sizni ochish";
+	Razban4.textContent = "kichik bir haq evaziga";
+	Razmut.textContent = "Chat ochish";
+	Razmut1.textContent = "bilan bir oy olib tashlang";
+	Razmut2.textContent = "sizning hisob!";
+	Razmut3.textContent = "chatda muloqot qilish";
+	Razmut4.textContent = "va jimgina o'ynang";
+	RazHS.textContent = "VK ochish";
+	RazHS1.textContent = 'Sutdan olib tashlang';
+	RazHS2.textContent = ' hech qanday dahshatli narsa!';
+	RazHS3.textContent = "sotib olish";
+	RazHS4.textContent = "va aloqada bo'ling!";
+	Perenos.textContent = "Tashish";
+	Perenos1.textContent = "ma'lumotlarni uzatish";
+	Perenos2.textContent = 'va imtiyoz';
+	Perenos3.textContent = "hisobingizdan";
+	Perenos4.textContent = "boshqa hisob";
+	Donat.textContent = "Ehson";
+	Donat1.textContent = 'sotib olish Donat,';
+	Donat2.textContent = 'va barcha orasida';
+	Donat3.textContent = "eng ko'p bo'l";
+	Donat4.textContent = "serverda salqin!";
+	VK.textContent = "texnik qo'llab-quvvatlash | VillageMC";
+	Сhoice.textContent = "tilni tanlash";
+}
+
+function USA() {
+	descProject1.textContent = ' VillageMC is the kindest server,';
+	descProject2.textContent = "that's why we give players a chance to fix it!";
+	contact.textContent = 'Our store! '
+	Razban.textContent = 'Unblocking';
+	Razban1.textContent = ' Did you realize your guilt?';
+	Razban2.textContent = ' Do you want to have fun? ';
+	Razban3.textContent = 'Unlock you';
+	Razban4.textContent = 'for a small fee';
+	Razmut.textContent = 'Unblocking Chat';
+	Razmut1.textContent = 'Remove mut from';
+	Razmut2.textContent = ' your account!';
+	Razmut3.textContent = 'Chat in a chat';
+	Razmut4.textContent = ' and play quietly ';
+	RazHS.textContent = 'Unblocking VK';
+	RazHS1.textContent = 'Got into an emergency VK? ';
+	RazHS2.textContent = " It's okay!";
+	RazHS3.textContent = ' Buy a different version ';
+	RazHS4.textContent = 'And stay in touch!';
+	Perenos.textContent = 'Transfer';
+	Perenos1.textContent = 'Transferring the data';
+	Perenos2.textContent = ' and privilege ';
+	Perenos3.textContent = 'from your account';
+	Perenos4.textContent = 'to another account';
+	Donat. textContent = 'Donate';
+	Donat1.textContent = 'Get a donat,';
+	Donat2.textContent = ' And among all ';
+	Donat3.textContent = 'be the most';
+	Donat4.textContent = ' cool on the server!';
+	VK.textContent = 'Technical Support | VillageMC';
+	Сhoice.textContent = 'Language selection';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
