afa0e597d77730c1f6c2d5bd777b18ccde1dcf6b
villagemc <46008022+villagemc@users.noreply.github.com>
2021-10-08 08:57:33 +0300

    Add files via upload

---
+++ LiquidBounce/LBRegHover.js
@@ -0,0 +1,73 @@
+let Fun = document.querySelector('.Fun')
+let listFun = document.querySelector('.listFun')
+let World = document.querySelector('.World')
+let listWorld = document.querySelector('.listWorld')
+let Targets = document.querySelector('.Targets')
+let listTargets = document.querySelector('.listTargets')
+let Misc = document.querySelector('.Misc')
+let listMisc = document.querySelector('.listMisc')
+let Player = document.querySelector('.Player')
+let listPlayer = document.querySelector('.listPlayer')
+let Movement = document.querySelector('.Movement')
+let listMovement = document.querySelector('.listMovement')
+let Render = document.querySelector('.Render')
+let listRender = document.querySelector('.listRender')
+let Exploit = document.querySelector('.Exploit')
+let listExploit = document.querySelector('.listExploit')
+let Combat = document.querySelector('.Combat')
+let listCombat = document.querySelector('.listCombat')
+
+Fun.onmouseover = () => {
+	listFun.style.display = 'block'
+}
+Fun.onmouseout = () => {
+	listFun.style.display = 'none'
+}
+World.onmouseover = () => {
+	listWorld.style.display = 'block'
+}
+World.onmouseout = () => {
+	listWorld.style.display = 'none'
+}
+Targets.onmouseover = () => {
+	listTargets.style.display = 'block'
+}
+Targets.onmouseout = () => {
+	listTargets.style.display = 'none'
+}
+Misc.onmouseover = () => {
+	listMisc.style.display = 'block'
+}
+Misc.onmouseout = () => {
+	listMisc.style.display = 'none'
+}
+Player.onmouseover = () => {
+	listPlayer.style.display = 'block'
+}
+Player.onmouseout = () => {
+	listPlayer.style.display = 'none'
+}
+Movement.onmouseover = () => {
+	listMovement.style.display = 'block'
+}
+Movement.onmouseout = () => {
+	listMovement.style.display = 'none'
+}
+Render.onmouseover = () => {
+	listRender.style.display = 'block'
+}
+Render.onmouseout = () => {
+	listRender.style.display = 'none'
+}
+Exploit.onmouseover = () => {
+	listExploit.style.display = 'block'
+}
+Exploit.onmouseout = () => {
+	listExploit.style.display = 'none'
+}
+Combat.onmouseover = () => {
+	listCombat.style.display = 'block'
+}
+Combat.onmouseout = () => {
+	listCombat.style.display = 'none'
+}
